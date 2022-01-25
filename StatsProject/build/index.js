1fdf5fb5e798d7745a6d7697be31dc7aae6ac2f9
rupamswain1 <rupamswain1@gmail.com>
2022-01-26 00:06:48 +0530

    csv reader added

--- StatsProject/build/index.js
+++ StatsProject/build/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('Hello!!!!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+}).split('\n')
+    .map(data => data.split(','));
+let count = 0;
+for (let data of matches) {
+    if ((data[1] === 'Man United' && data[5] === 'H') || (data[2] === 'Man United' && data[5] === 'A')) {
+        count++;
+    }
+}
+console.log(`Man United has won ${count} matches`);
--- StatsProject/src/index.ts
+++ StatsProject/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello!!!!');
+import fs from 'fs'
+
+const matches=fs.readFileSync('football.csv',{
+    encoding:'utf-8'
+}).split('\n')
+.map(data=>data.split(','))
+let count=0;
+for(let data of matches){
+    
+    if((data[1]==='Man United' && data[5]==='H')||(data[2]==='Man United' && data[5]==='A')){
+        count++;
+    }
+   
+}
+console.log(`Man United has won ${count} matches`);
